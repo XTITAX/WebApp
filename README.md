2702f5d4196e59342d09e79a66d7ffd6283ad221
XTITAX <xtitax@yandex.ru>
2024-07-16 12:24:16 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# TiLauncher
+
+WebApp - Это библиотека для запуска локального веб приложения.
+
+## Установка
+
+Вы можете установить WebApp с помощью pip:
+
+```bath
+python -m pip install git+https://github.com/XTITAX/WebApp.git
+```
+
+## Использование
+
+Импортируйте функции из пакета и вызовите их:
+
+```python
+import WebApp
+
+# Запустить локальное веб приложение
+TiLauncher.WebSite(Путь к странице)
+```
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='WebApp',
+    version='1.0',
+    author='XTITAX',
+    author_email='xtitax@yandex.ru',
+    description='Библиотека для веб приложения',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/XTITAX/WebApp',
+    packages=find_packages(include=['WebApp']),
+    include_package_data=True,
+    install_requires=[
+        'webbrowser',
+    ],
+    package_data={
+        'TiLauncher': ['jdk-22', '.gitignore'],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
