06b56ffc7a2c9f2b25e33cb88fdf4e31cfe42029
XTITAX <xtitax@yandex.ru>
2024-07-16 18:04:46 +0300

    Update WebApp.py

--- WebApp/WebApp.py
+++ WebApp/WebApp.py
@@ -2,6 +2,11 @@ import subprocess
 import webbrowser
 
 def WebSite(filepath, folderpath):
-    subprocess.run(["@echo", "off"], shell=True)
-    subprocess.run([f"jdk-22\\bin\\jwebserver.exe -d {folderpath}"], shell=True)
-    webbrowser.open_new("http://127.0.0.1:8000" + filepath)
+    # Запускаем веб-сервер с помощью subprocess
+    subprocess.run([r"jdk-22\bin\jwebserver.exe", "-d", folderpath], shell=True)
+
+    # Формируем URL для открытия веб-страницы
+    url = f"http://127.0.0.1:8000/{filepath}"
+
+    # Открываем URL в браузере
+    webbrowser.open_new(url)
